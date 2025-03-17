37c89d37ce76002134fdd7f891747e9a99d449e7
Mauro Pascale <mauroppascalebbrum@gmail.com>
2025-03-17 16:04:22 -0300

    Agrega los archivos Investigador.cpp y Investigador.h

---
+++ Investigador.cpp
@@ -0,0 +1,45 @@
+#include "Investigador.h"
+
+
+Investigador::Investigador(std::string ORCID, std::string nombre, std::string institucion) : ORCID(ORCID), nombre(nombre), institucion(institucion){
+}
+
+void Investigador::setORCID(std::string orc){
+    ORCID = orc;
+}
+
+void Investigador::setNombre(std::string nom){
+    nombre = nom;
+}
+
+void Investigador::setInstitucion(std::string ins){
+    institucion = ins;
+}
+
+void Investigador::setPublicacion(Publicacion& pub){ 
+    publicaciones.push_back(&pub);
+}
+
+std::string Investigador::getORCID(){
+    return ORCID;
+}
+std::string Investigador::getNombre(){
+    return nombre;
+}
+std::string Investigador::getInstitucion(){
+    return institucion;
+}
+
+std::list<std::string> Investigador::listarPublicaciones(DTFecha desde, std::string palabra){
+    std::list<std::string> res;
+    for(std::list<Publicacion*>::iterator it = publicaciones.begin(); it != publicaciones.end(); it++){
+        if((*it)->getFecha().comparar(desde) == 1 && (*it)->contienePalabra(palabra)){
+            res.push_back((*it)->getDOI());
+        } 
+    }
+    return res;
+}
+
+void Investigador::toString(){
+   std::cout << ORCID << "->" << nombre << "/" << institucion << "."<< std::endl;
+}
---
+++ Investigador.h
@@ -0,0 +1,32 @@
+#include <string>
+#include <iostream>
+#include <list>
+#include "DTFecha.h"
+#include "publicacion.h"
+
+
+
+#ifndef INVESTIGADOR_H
+#define INVESTIGADOR_H
+
+class Investigador{
+    private:
+      std::string ORCID;
+      std::string nombre;
+      std::string institucion;
+      std::list<Publicacion*> publicaciones; //lista de punteros a publicaciones
+    
+    public:
+      Investigador(std::string, std::string, std::string);
+      void setORCID(std::string);
+      void setNombre(std::string);
+      void setInstitucion(std::string);
+      void setPublicacion(Publicacion&);
+      std::string getORCID();
+      std::string getNombre();
+      std::string getInstitucion();
+      std::list<std::string> listarPublicaciones(DTFecha, std::string); //lista de string con doi de publicaciones
+      void toString();
+};
+
+#endif INVESTIGADOR_H
